dedbad328e0936fb63f238683a203277f4f91460
JeremyRand <jeremyrand@airmail.cc>
2019-01-25 20:55:05 +0000

    Truncate AuxPoW for block heights covered by a checkpoint (#710)

--- electrumx/lib/coins.py
+++ electrumx/lib/coins.py
@@ -46,7 +46,7 @@ import electrumx.lib.tx_dash as lib_tx_dash
 import electrumx.server.block_processor as block_proc
 import electrumx.server.daemon as daemon
 from electrumx.server.session import (ElectrumX, DashElectrumX,
-                                      SmartCashElectrumX)
+                                      SmartCashElectrumX, AuxPoWElectrumX)
 
 
 Block = namedtuple("Block", "raw header transactions")
@@ -272,6 +272,7 @@ class Coin(object):
 class AuxPowMixin(object):
     STATIC_BLOCK_HEADERS = False
     DESERIALIZER = lib_tx.DeserializerAuxPow
+    SESSIONCLS = AuxPoWElectrumX
 
     @classmethod
     def header_hash(cls, header):
