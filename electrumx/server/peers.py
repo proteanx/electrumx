4e103a93e0a91eeb49e3b4e47322ddabb1b39336
Calin Culianu <calin.culianu@gmail.com>
2019-04-14 00:34:43 +0300

    added a comment

--- electrumx/server/peers.py
+++ electrumx/server/peers.py
@@ -289,6 +289,8 @@ class PeerManager(object):
         return False
 
     def _dupes_for_peer(self, peer_ip_addr):
+        ''' Note: peer_ip_addr in this case is an ip address string as returned
+        from getaddrinfo and not a Python ip_address object. '''
         dupes = [p for p in self.peers.copy() if p.last_good and not p.bad and not p.is_tor and p.ip_addr == peer_ip_addr]
         return dupes
 
